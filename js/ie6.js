7bddc3d1e839656fd59d2bdbac2a17f1584846f6
ZHENG Huiqi <grey220022@hotmail.com>
2011-11-17 15:10:13 +0100

    First commit

---
+++ js/ie6.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+	DD_belatedPNG.fix(".transparent,img,#contentTop,#contentBottom,#contentM,#adsBanner #adsBannerContent .navC,.greenBtnBig,.greenBtnBig .right,.greenBtnBig .middle,#adsBanner ul li.first,#adsBanner ul li.last,#adsBanner .adsNav,.contentWrapper .sectionWrapper,.sideBar,.pager ul a,.pager ul li.current a,.greenBtn,.greenBtn .right,.greenBtn .middle,#skinSwitchWrapper,#loginBoxT,#loginBoxB,#loginBoxM,.grayBtn,.grayBtn .right,.grayBtn .middle,.confirmSection,.videoContainer a span,.article .title .hideIcon,.article .title .showIcon,.blogList .dateTime");
+	
+});
